516e58410335d7223fc32a1c36f7b32296d27117
AkbarMT58 <taufiksakbar@gmail.com>
2023-02-22 05:38:47 +0700

    update ke server
    update pagi

---
+++ src/pages/LogisticChina/Repacking.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+//import RepackingTable from '../../components/LogisticChina/Repacking/RepackingTable';
+import RepackingTableTry from '../../components/LogisticChina/Repacking/RepackingTableTry';
+
+
+
+const Repacking = () => {
+ 
+  return (
+  
+      <RepackingTableTry />
+  
+  );
+
+
+};
+
+export default Repacking;
